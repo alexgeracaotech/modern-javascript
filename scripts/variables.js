9b5e8ec41b1b4cf885d0fd9e9fe49edeedad5a54
Alex Bessa <alexbessa.dev@gmail.com>
2025-08-16 09:05:21 -0300

    problemas com içamento

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,14 +1,14 @@
-// var aceita redeclaração e reatribuição
+
+// var aceita redeclaração e reatribuição (sofre içamento)
 var nome = "Alex";
 var nome = "Arthur";
-console.log(profissao);
+console.log(nome);
 
-// let não aceita redeclaração, mas aceita reatribuição
+// let não aceita redeclaração, mas aceita reatribuição (não sofre içamento)
 let idade = 29;
 idade = 27;
 console.log(idade);
 
-// const não aceita redeclaração nem reatribuição
+// const não aceita redeclaração nem reatribuição (não sofre içamento)
 const profissao = "desenvolvedor";
 console.log(profissao);
-
