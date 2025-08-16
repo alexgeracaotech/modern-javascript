5846654a3b86d3118b8cb08025b3f5a00c3aacec
Alex Bessa <alexbessa.dev@gmail.com>
2025-08-16 10:45:34 -0300

    trabalhando com arraow functions

---
+++ scripts/arrow-functions.js
@@ -0,0 +1,22 @@
+
+// // forma tradidicional de expressão de uma arrow function
+// const saudar = () => {
+//   console.log("Olá!");
+// };
+
+// saudar();
+
+// const multiplicar = (numero) => {
+//   return numero * 2;
+// };
+
+// const resultado = multiplicar(10);
+
+// console.log(resultado);
+
+// // forma curta e consisa de uma arrow function
+// const multiplicar = numero => numero * 2;
+
+// const resultado = multiplicar(10);
+
+// console.log(resultado);
